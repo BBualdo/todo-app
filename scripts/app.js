0a160829d9793ae3bd578f3fa76bee63dc00628e
Sebastian <118930386+BBualdo@users.noreply.github.com>
2023-08-22 09:23:47 +0200

    (Concept) Added „completed” property to todoList

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,28 @@
 let todoList =  /* JSON.parse(localStorage.getItem('todoList')) || */[
-  {name: 'Complete online JavaScript course'},
-  {name: 'Jog around the park 3x'},
-  {name: '10 minutes meditation'},
-  {name: 'Read for 1 hour'},
-  {name: 'Pick up groceries'},
-  {name: 'Complete Todo App on Frontend Mentor'}
+  {
+    name: 'Complete online JavaScript course',
+    completed: false
+  },
+  {
+    name: 'Jog around the park 3x',
+    completed: false
+  },
+  {
+    name: '10 minutes meditation',
+    completed: false
+  },
+  {
+    name: 'Read for 1 hour',
+    completed: false
+  },
+  {
+    name: 'Pick up groceries',
+    completed: false
+  },
+  {
+    name: 'Complete Todo App on Frontend Mentor',
+    completed: false
+  }
 ];
 
 renderTodoList();
@@ -57,8 +75,8 @@ document.querySelector('.todo')
 function addTodo() {
   const inputElement = document.querySelector('.todo');
   const name = inputElement.value
-  
-  todoList.push({name});
+  const complition = todoList.completed
+  todoList.push({name, complition = false});
   
   inputElement.value = '';
 
